732843f29a3a8b698c926bfedef49301b8d3291c
tarunjha16-ship-it <tarunjha16@gmail.com>
2026-06-06 17:16:18 +0530

    Add student marks analysis functionality

---
+++ 02_student_marks_analyzer.py
@@ -0,0 +1,6 @@
+marks = [85, 92, 78, 67, 88]
+
+print("Total Students:", len(marks))
+print("Highest Mark:", max(marks))
+print("Lowest Mark:", min(marks))
+print("Average Mark:", sum(marks)/len(marks))
