848abf594c6f94c013464c2197670e83a09ea07d
tarunjha16-ship-it <tarunjha16@gmail.com>
2026-06-09 14:48:34 +0530

    Add student list and print names with marks

---
+++ 17_student_list_of_dictionaries.py
@@ -0,0 +1,8 @@
+students = [
+    {"Name": "Ram", "Marks": 80},
+    {"Name": "Mohan", "Marks": 90},
+    {"Name": "Sita", "Marks": 75}
+]
+
+for student in students:
+    print(student["Name"], student["Marks"])
